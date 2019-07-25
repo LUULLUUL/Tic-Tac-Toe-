300c9ab403e6152d603009edc4539f0150d22214
LU Jiaying <lujiaying7@gmail.com>
2019-07-25 10:59:01 +0200

    first commit

---
+++ lib/Board.rb
@@ -0,0 +1,20 @@
+
+# TO DO : la classe a 1 attr_accessor : un array/hash qui contient les BoardCases.
+# Optionnellement on peut aussi lui rajouter un autre sous le nom @count_turn pour compter le nombre de coups joué
+
+
+
+  
+  
+
+  def display_board
+    @board = [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ']
+    puts "#{@board[0]} | #{@board[1]} | #{@board[2]}"
+    puts "-"*10
+    puts "#{@board[3]} | #{@board[4]} | #{@board[5]}"
+    puts "-"*10
+    puts "#{@board[6]} | #{@board[7]} | #{@board[8]}"
+  end
+  display_board
+ 
+
---
+++ lib/BoardCase.rb
@@ -0,0 +1,36 @@
+
+  #TO DO : la classe a 2 attr_accessor, sa valeur en string (X, O, ou vide), ainsi que son identifiant de case
+
+  attr_accessor :symbol, :box_position
+
+  def initialize(symbol, box_position)
+    @symbol = 
+    @box_position = []
+  end  
+
+  winner_position =[
+   [0,1,2]
+   [3,4,5]
+   [6,7,8]
+   [0,3,6]
+   [1,4,7]
+   [2,5,8]
+   [0,4,8]
+   [2,4,6]
+  ]
+
+
+
+  
+
+
+  
+  # def initialize
+  #   #TO DO : doit régler sa valeur, ainsi que son numéro de case
+  # end
+  
+  
+
+
+  
+end
